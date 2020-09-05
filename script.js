07d9e41fca38d7ef40656b4e5685949e69164453
vfonsah <vitalis.fonsah@gmail.com>
2020-09-05 20:37:59 +0100

    random colors for divs

--- script.js
+++ script.js
@@ -2,50 +2,70 @@
 const gridContainer = document.createElement("div");
 const buttonsContainer = document.createElement("div");
 const newGridBtn = document.createElement("button");
+const clearGridBtn = document.createElement('button')
+let color = [];
 
 // element classes
 gridContainer.classList.add("container");
-buttonsContainer.classList.add('row');
+buttonsContainer.classList.add("row");
 
 // element IDs
-newGridBtn.id = 'newGridBtn';
+newGridBtn.id = "newGridBtn";
 
 // element textContents
-newGridBtn.textContent = "New Grid"
+newGridBtn.textContent = "New Grid";
+clearGridBtn.textContent="Clear Grid";
 
 // event listeners
 newGridBtn.addEventListener("click", () => {
   let newGrid = prompt("Enter a new size for the grid", "");
   while (newGrid < 16 || newGrid > 100) {
-      newGrid = prompt("You grid size must be between 16 & 100", "");
+    newGrid = prompt("You grid size must be between 16 & 100", "");
   }
   if (newGrid >= 16 && newGrid <= 100) {
-    makeNewGrid(newGrid);
-  } 
+    clearGrid(makeNewGrid(newGrid));
+  }
 });
+
+clearGridBtn.onclick = () => {
+    clearGrid();
+}
 
 // event functions
 //   page functions
-function pixelate(e) {
-  e.target.classList.add("pixelate");
-}
-
 
 function makeGrid(rows, cols) {
-    gridContainer.style.setProperty("--grid-cols", cols);
-    gridContainer.style.setProperty("--grid-rows", rows);
-    
-    for (let i = 0; i < rows * cols; i++) {
-        var cell = document.createElement("div");
-        cell.id = `grid-cell-${i + 1}`;
-        gridContainer.appendChild(cell).className = "grid-cell";
-        cell.addEventListener("mouseover", pixelate);
-    }
-    
-    return cell;
+  gridContainer.style.setProperty("--grid-cols", cols);
+  gridContainer.style.setProperty("--grid-rows", rows);
+
+  for (let i = 0; i < rows * cols; i++) {
+    var cell = document.createElement("div");
+    cell.id = `grid-cell-${i + 1}`;
+    gridContainer.appendChild(cell).className = "grid-cell";
+    cell.addEventListener("mouseover", pixelate);
+  }
+
+  return cell;
 }
 
-const makeNewGrid = (gridSize) => makeGrid(gridSize, gridSize);
+const pixelate = (e) => {
+  for (let rgb = 0; rgb <= 2; rgb++) {
+    color[rgb] = Math.floor(Math.random() * Math.floor(255));
+  }
+  e.target.style.backgroundColor = `rgb(${color})`;
+};
+
+
+const makeNewGrid = (gridSize) => {
+  makeGrid(gridSize, gridSize);
+};
+
+const clearGrid = (newFun) => {
+  let gridElements = gridContainer.childNodes;
+  gridElements.forEach((divs) => {
+    divs.style.backgroundColor = '#ffffff';
+  });
+};
 
 const grid = newGridBtn.onclick
   ? (gridSize) => makeNewGrid(gridSize)
@@ -54,7 +74,7 @@ const grid = newGridBtn.onclick
 //   compile page elements
 
 gridContainer.appendChild(grid);
-buttonsContainer.appendChild(newGridBtn);
+buttonsContainer.append(newGridBtn, clearGridBtn);
 
 const domBody = document.querySelector("body");
-domBody.append(buttonsContainer, gridContainer)
+domBody.append(buttonsContainer, gridContainer);
