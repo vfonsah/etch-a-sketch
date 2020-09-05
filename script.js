dcd66b5db248811ff463e962c3eb4326d50bb7fc
vfonsah <vitalis.fonsah@gmail.com>
2020-09-05 18:20:56 +0100

    change bg-color and create new grid

--- script.js
+++ script.js
@@ -1,22 +1,60 @@
-const container = document.createElement("div");
-container.classList.add("container");
+// constants & variables
+const gridContainer = document.createElement("div");
+const buttonsContainer = document.createElement("div");
+const newGridBtn = document.createElement("button");
 
-// functions
-function makeGrid(rows, cols) {
-  container.style.setProperty("--grid-cols", cols);
-  container.style.setProperty("--grid-rows", rows);
+// element classes
+gridContainer.classList.add("container");
+buttonsContainer.classList.add('row');
 
-  for (let i = 0; i < rows * cols; i++) {
-    var cell = document.createElement("div");
-    cell.id = `grid-cell-${i + 1}`;
-    container.appendChild(cell).className = "grid-cell";
+// element IDs
+newGridBtn.id = 'newGridBtn';
+
+// element textContents
+newGridBtn.textContent = "New Grid"
+
+// event listeners
+newGridBtn.addEventListener("click", () => {
+  let newGrid = prompt("Enter a new size for the grid", "");
+  while (newGrid < 16 || newGrid > 100) {
+      newGrid = prompt("You grid size must be between 16 & 100", "");
   }
+  if (newGrid >= 16 && newGrid <= 100) {
+    makeNewGrid(newGrid);
+  } 
+});
 
-  return cell;
+// event functions
+//   page functions
+function pixelate(e) {
+  e.target.classList.add("pixelate");
 }
-const grid = makeGrid(16, 16);
 
-container.appendChild(grid);
+
+function makeGrid(rows, cols) {
+    gridContainer.style.setProperty("--grid-cols", cols);
+    gridContainer.style.setProperty("--grid-rows", rows);
+    
+    for (let i = 0; i < rows * cols; i++) {
+        var cell = document.createElement("div");
+        cell.id = `grid-cell-${i + 1}`;
+        gridContainer.appendChild(cell).className = "grid-cell";
+        cell.addEventListener("mouseover", pixelate);
+    }
+    
+    return cell;
+}
+
+const makeNewGrid = (gridSize) => makeGrid(gridSize, gridSize);
+
+const grid = newGridBtn.onclick
+  ? (gridSize) => makeNewGrid(gridSize)
+  : makeGrid(16, 16);
+
+//   compile page elements
+
+gridContainer.appendChild(grid);
+buttonsContainer.appendChild(newGridBtn);
 
 const domBody = document.querySelector("body");
-domBody.appendChild(container);
+domBody.append(buttonsContainer, gridContainer)
